dca4f6d8a8082e3700b92f23d56e5c6f91dd91e7
aselims <selim.2k@gmail.com>
2015-06-08 17:39:50 +0200

    clean...

--- app/src/main/java/co/rahala/selim/rocketinternet1/network/APIInterface.java
+++ app/src/main/java/co/rahala/selim/rocketinternet1/network/APIInterface.java
@@ -13,23 +13,11 @@ import retrofit.http.Query;
  */
 public interface APIInterface {
 
-    /*@GET("/?method=artist.gettopalbums&api_key=01590856c264cfd360839494df12dec5&format=json")
-    Album1 album1(@Query("artist") String name);*/
 
     //    //https://www.zalora.com.my/mobile-api/women/clothing?maxitems=24&page=1&sort=price&dir=desc
 
     @GET("/?maxitems=24&page=1")
     void getItems(@Query("sort") String sort, @Query("dir") String dir, Callback<Items> cb);
 
-    /*//http://api.goeuro.com/api/v2/position/suggest/{locale}/{term}
-    @GET("/{locale}/{term}")
-    void getPlaces(@Path("locale") String locale, @Path("term") String term, Callback<List<Place>> cb);
 
-*/
-   /* @GET("/?method=artist.gettopalbums&api_key=01590856c264cfd360839494df12dec5&format=json")
-    void getAlbums(@Query("artist") String name, Callback<Album1> cb);*/
-
-   /* @POST("/")
-    void sendBarcode(@Body Barcode body, Callback<BarcodeData> callBack);
-*/
 }
